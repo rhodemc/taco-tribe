42f2826478cbbab5e7f1f1480e0c87976b58182f
rhodemc <rhodemc@gmail.com>
2023-05-04 21:00:36 -0700

    paths

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,34 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { BlogPost, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    // const projectData = await Project.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
+
+    // // Serialize data so the template can read it
+    // const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('landing', {
+      // logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// router.get('/dashboard', async (req, res) => {
 //   try {
-//     // Get all projects and JOIN with user data
-//     const projectData = await Project.findAll({
+//     const dashboardData = await dashboard.findAll(req.params.id, {
 //       include: [
 //         {
 //           model: User,
@@ -14,61 +37,35 @@ const withAuth = require('../utils/auth');
 //       ],
 //     });
 
-//     // Serialize data so the template can read it
-//     const projects = projectData.map((project) => project.get({ plain: true }));
+//     const dashboard = dashboardData.get({ plain: true });
 
-//     // Pass serialized data and session flag into template
-//     res.render('homepage', {
-//       projects,
-//       logged_in: req.session.logged_in
+//     res.render('dashboard', {
+//       ...dashboard,
+//       logged_in: req.session.logged_in,
 //     });
 //   } catch (err) {
 //     res.status(500).json(err);
 //   }
 // });
 
-router.get('/dashboard', async (req, res) => {
+// withAuth middleware added to route
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
-    const dashboardData = await dashboard.findAll(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: BlogPost }],
     });
 
-    const dashboard = dashboardData.get({ plain: true });
+    const user = userData.get({ plain: true });
 
     res.render('dashboard', {
-      ...dashboard,
-      logged_in: req.session.logged_in,
+      ...user,
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/', async (req, res) => {
-  // try {
-  //   // Find the logged in user based on the session ID
-  //   const userData = await User.findByPk(req.session.user_id, {
-  //     attributes: { exclude: ['password'] },
-  //     include: [{ model: BlogPost }],
-  //   });
-
-  //   const user = userData.get({ plain: true });
-
-  //   res.render('dashboard', {
-  //     ...user,
-  //     logged_in: true
-  //    });
-  // } catch (err) {
-  //   console.log(err);
-  //   res.status(500).json(err);
-  // }
-  res.render('landing');
 });
 
 router.get('/login', (req, res) => {
